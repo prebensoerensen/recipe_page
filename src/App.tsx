1b47f40b65ff8e6e29608da417a06e22fa53e62a
PrebenS <prebens.kodehode@gmail.com>
2023-09-03 21:00:29 +0200

    added content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecipeCard } from "./components/RecipeCard/RecipeCard";
+import { LayoutBase } from "./Layouts/LayoutBase/LayoutBase";
+import { recipes } from "./data/Recipes";
+
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <LayoutBase>
+      <section className="recipes-section">
+        {recipes.map((recipe) => (
+          <RecipeCard
+            key={recipe.id}
+            id={recipe.id}
+            name={recipe.name}
+            servings={recipe.servings}
+            timeToMake={recipe.timeToMake}
+            ingredients={recipe.ingredients}
+            directions={recipe.directions}
+          />
+        ))}
+      </section>
+    </LayoutBase>
+  );
 }
 
-export default App
+export default App;
